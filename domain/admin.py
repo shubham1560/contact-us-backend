0db6715b1241da953d24e3bfcbbc197d288c6786
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-09-01 03:26:19 +0530

    Added services to the domain application

--- domain/admin.py
+++ domain/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Domain
 # Register your models here.
+
+
+class DomainAdmin(admin.ModelAdmin):
+    model = Domain
+    list_display = ['name', 'active', 'parent', 'domain_code', 'domain_path']
+    fields = ['name', 'active', 'description', 'parent']
+    ordering = ['-sys_created_on']
+
+
+admin.site.register(Domain, DomainAdmin)
--- domain/models.py
+++ domain/models.py
@@ -14,10 +14,18 @@ class Domain(models.Model):
     default = models.BooleanField(default=False)
     description = models.TextField(null=True, blank=True)
     name = models.CharField(max_length=100, blank=True, null=True)
-    parent = models.ForeignKey('self', blank=True, null=True)
+    parent = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE)
     primary = models.BooleanField(default=False, blank=True, null=True)
     sys_created_on = models.DateTimeField(auto_now_add=True)
     sys_updated_on = models.DateTimeField(auto_now=True)
     domain_code = models.CharField(max_length=6)
     domain_path = models.CharField(max_length=256)
     type = models.CharField(choices=DOMAIN_TYPE, max_length=2, default="cr")
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+
+        super().save(*args, **kwargs)
+
