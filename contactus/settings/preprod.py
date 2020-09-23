d1a46e9e86ca4908416e24cbc1ac9d9ee7648bce
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-09-23 16:20:47 +0530

    Readying it for production

---
+++ contactus/settings/preprod.py
@@ -0,0 +1,20 @@
+
+DATABASES = {
+
+        'default': {
+
+            'ENGINE': 'django.db.backends.postgresql',
+
+            'NAME': 'd96o8q660230lc',
+
+            'USER': 'nnsktfzlooqgzz',
+
+            'PASSWORD': 'df133e44e9d9954242731fa747f3e3b7dc2758f775165bf36e09f6aa38e4d37a',
+
+            'HOST': 'ec2-184-73-249-9.compute-1.amazonaws.com',
+
+            'PORT': '5432',
+
+        }
+
+    }
--- contactus/wsgi.py
+++ contactus/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+from decouple import config
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contactus.settings.base')
+if config('LIVE') == "FALSE":
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contactus.settings.dev')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contactus.settings.final')
 
 application = get_wsgi_application()
