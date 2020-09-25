52bbc16d3e7fcfef5d29f5a65f8ecaa09e42e457
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-09-25 08:11:45 +0530

    Added cors header for http communication

--- contactus/settings/base.py
+++ contactus/settings/base.py
@@ -46,20 +46,52 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'domain_preference.apps.DomainPreferenceConfig',
-    'storages'
+    'storages',
+    'corsheaders',
 ]
 
 AUTH_USER_MODEL = 'sys_user.SysUser'
 
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:4200",
+    # "https://uf-front.web.app",
+    # "https://uf-front.firebaseapp.com",
+    # "https://sortedtree.com"
+]
+
+CORS_ALLOW_HEADERS = [
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
 
 
 REST_FRAMEWORK = {
