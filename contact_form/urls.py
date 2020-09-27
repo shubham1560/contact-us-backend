904d86c435e28c4b86d70a471b6a9937e51425e7
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-09-28 02:59:19 +0530

    Added functionality to change the page lead

--- contact_form/urls.py
+++ contact_form/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ContactUsListView, ContactUsPostView, ContactUsChangeView
 
 urlpatterns = [
-    path('v1/contact-us/get/<int:start>/<str:type>/', ContactUsListView.as_view()),
+    path('v1/contact-us/get/<int:start>/<str:message_type>/', ContactUsListView.as_view()),
     path('v1/contact-us/post/', ContactUsPostView.as_view()),
     path('v1/contact-us/change/', ContactUsChangeView.as_view())
 ]
